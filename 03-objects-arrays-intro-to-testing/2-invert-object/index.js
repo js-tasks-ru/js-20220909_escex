b5e35cd3a119ba3023bee433097400817a2736a8
Sergey Karasev <escex@mail.ru>
2022-09-18 20:40:47 +0300

    feat: реализация функции замены ключ-значение в объекте

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,11 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  if (typeof obj === 'object') {
+    let newObj = {};
+    for (let [key, value] of Object.entries(obj)) {
+      newObj[value] = key;
+    }
+    return newObj;
+  }
 }
