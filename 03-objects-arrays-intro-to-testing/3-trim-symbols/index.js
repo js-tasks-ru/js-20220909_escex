e47b05e8d0db1aea46d32e733b856e38f0b850b5
Sergey Karasev <escex@mail.ru>
2022-09-19 11:10:46 +0300

    feat: реализация функции удаления символов

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,32 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  if (!string || size === 0) { 
+    return '';
+  } else if (size === undefined) {
+    return string; 
+  } else {
+    const symbols = string.split('');
+    const tempArray = [[]];
+    const result = [];
+    let indexArray = 0;
+    symbols.forEach((currentItem, index) => {
+      let nextItem = symbols.at(index + 1);
+      if (currentItem === nextItem) {
+        tempArray[indexArray].push(currentItem);        
+      } else {
+        tempArray[indexArray].push(currentItem);
+        if (nextItem !== undefined) {
+          tempArray.push([]);
+          indexArray += 1;
+        }
+      }
+    });
+    tempArray.forEach(item=>{
+      let currSize = item.length > size ? size : item.length;
+      result.push(...Array(Number(currSize)).fill(item[0]));
+    });
+    return result.join('');
+  }
+  
 }
